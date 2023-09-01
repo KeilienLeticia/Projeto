d4995c55d4a51091740ead4e28efc90bb4412eb4
KeilienLeticia <107710943+KeilienLeticia@users.noreply.github.com>
2023-08-31 23:37:35 -0400

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,3 @@
+﻿int var, var1, var2;
+Console.WriteLine("Informe o primeiro Numero");
+var=Convert.ToInt32(var);
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿int num1, num2, num3;
+
+Console.WriteLine("Informe o primeiro numero");
+
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o segundo numero");
+
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o terceiro numero");
+
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2 && num1 < num3)
+
+{
+
+    if (num2 < num3)
+
+    {
+
+        Console.WriteLine(num1 + " " + num2 + " " + num3);
+
+    }
+
+    else
+
+    {
+
+        Console.WriteLine(num1 + " " + num3 + " " + num2);
+
+    }
+
+}
+
+else
+
+{
+
+    if (num2 < num1 && num2 < num3)
+
+    {
+
+        if (num1 < num3)
+
+        {
+
+            Console.WriteLine(num2 + " " + num1 + " " + num3);
+
+        }
+
+        else
+
+        {
+
+            Console.WriteLine(num2 + " " + num3 + " " + num1);
+
+        }
+
+    }
+
+}
